fb8be7bd6dbe015957f15aa2b5b5c367c2216262
Ryan Troxler <rtroxler@ksu.edu>
2012-08-11 15:26:50 -0500

    Initial setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lightweight::Application.config.secret_token = '3bda6a44dec7b5370c27a0a11d539bfc17d54f31ab18a14655ff17d3d52992ead57def32e27195e2de0b46ae81879592677358f751566d19075228ca029dddaf'
