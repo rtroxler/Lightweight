4f7c38c1ff76891f09ddf3f27e34f42cb9a54e80
Ryan Troxler <rtroxler@ksu.edu>
2012-08-12 13:21:12 -0500

    Title helper.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,15 @@ describe "Static pages" do
         page.should have_content('Lightweight')
     end
 
-    it "Should have the right title.." do
+    it "Should have the base title.." do
         visit '/static_pages/home'
         page.should have_selector('title', 
-                                  :text => "Lightweight | Home")
+                                  :text => "Lightweight")
+    end
+
+    it "should not have a custom page title" do
+        visit '/static_pages/home'
+        page.should_not have_selector('title', :text => '| Home')
     end
   end
 
